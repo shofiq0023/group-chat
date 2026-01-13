2f6aceaaecdb4b423f8431802f9ca41648df1ed6
shofiq0023 <shofiqulislam0023@gmail.com>
2026-01-14 00:26:41 +0600

    updated message dto

--- java-socket-server/src/main/java/com/shofiqul/socket/config/SocketModule.java
+++ java-socket-server/src/main/java/com/shofiqul/socket/config/SocketModule.java
@@ -2,6 +2,7 @@ package com.shofiqul.socket.config;
 
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.shofiqul.socket.dto.BroadcastMessageDto;
 import com.shofiqul.socket.dto.MessageDto;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
@@ -12,10 +13,12 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class SocketModule {
     private final Logger log = LoggerFactory.getLogger(SocketModule.class);
+    private final String ROOM_BROADCAST = "rm_broadcast";
 
     @Autowired
     private SocketIOServer server;
@@ -33,13 +36,16 @@ public class SocketModule {
     }
 
     public void addEventListener() {
-        Map<String, SocketIOClient> users = new HashMap<>();
+        Map<String, SocketIOClient> users = new ConcurrentHashMap<>();
 
         server.addConnectListener(client -> {
             client.sendEvent("connected", "Socket connected!");
             String username = client.getHandshakeData().getSingleUrlParam("username");
             users.put(username, client);
+            client.joinRoom(ROOM_BROADCAST);
+
             log.info("{} client connected!", client.getSessionId());
+            log.info("{} client joined the room: {}", client.getSessionId(), ROOM_BROADCAST);
         });
 
         server.addDisconnectListener(client -> log.info("{} disconnected", client.getSessionId()));
@@ -47,6 +53,11 @@ public class SocketModule {
         server.addEventListener("message", MessageDto.class, (client, message, request) -> {
             SocketIOClient toClient = users.get(message.getToUsername());
             toClient.sendEvent("message", message);
+        });
+
+        server.addEventListener("broadcast_message", BroadcastMessageDto.class, (client, message, request) -> {
+            log.debug("message: {}", message);
+            server.getRoomOperations(ROOM_BROADCAST).sendEvent("broadcast_message", message);
         });
     }
 }
--- java-socket-server/src/main/java/com/shofiqul/socket/dto/BroadcastMessageDto.java
+++ java-socket-server/src/main/java/com/shofiqul/socket/dto/BroadcastMessageDto.java
@@ -1,4 +1,18 @@
 package com.shofiqul.socket.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BroadcastMessageDto {
+    private String fromUsername;
+    private String message;
+    private Timestamp timestamp;
 }
