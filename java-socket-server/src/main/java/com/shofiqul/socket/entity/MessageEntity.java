33c02040a1c53250b43a5b12a25b3cb0eee8b425
shofiq0023 <shofiqulislam0023@gmail.com>
2026-02-03 23:37:21 +0600

    removed unused imports

--- java-socket-server/src/main/java/com/shofiqul/socket/entity/MessageEntity.java
+++ java-socket-server/src/main/java/com/shofiqul/socket/entity/MessageEntity.java
@@ -3,11 +3,9 @@ package com.shofiqul.socket.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
-import java.sql.Timestamp;
 import java.util.HashSet;
 import java.util.Set;
 
